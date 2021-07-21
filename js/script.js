eea308955f63947d7f092713e0b16c64c8e3dbde
DESKTOP-5L8JNM3\LINE <oidolee@gmail.com>
2021-07-21 12:14:44 +0900

    사진추가

--- js/script.js
+++ js/script.js
@@ -9,12 +9,27 @@ $(function(){
         coupons[i].addEventListener('click',()=>{
             loginBg.classList.add('login_Bgpop');
              loginBg.style.top = scrollY + 'px' ;
+             login.classList.add('login_effect');
+             login.style.top = scrollY + 'px' ;
         });
-        coupons[i].addEventListener('click',()=>{
-            login.classList.add('login_effect');
-            login.style.top = scrollY + 'px' ;
-        });
+    
     });
     /*로그인 화면 팝업 효과 --------------end*/
 
+    /* 로그인 화면 닫기 start*/
+
+    const loginClose = document.querySelector('#login_close');
+    loginClose.addEventListener('click',()=>{
+        loginBg.classList.remove('login_Bgpop');
+        login.classList.remove('login_effect');
+    /* 로그인 화면 닫기 --------------end*/
+
+    });
+
+
+
+    
+
+    /* start*/
+    /* --------------end*/
 });
