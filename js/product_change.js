91e0b09361836d7ff7d7f995686925f2216541b4
yyhh1016 <yyhh1016@naver.com>
2021-07-23 10:17:06 +0900

    no message

--- js/product_change.js
+++ js/product_change.js
@@ -4,18 +4,22 @@ $(function(){
 
   
   $("#sec_sub1").click(function(){
+    $("#second_3").children().hide();
     $("#sec_box_1").show();
   });
 
   $("#sec_sub2").click(function(){
+    $("#second_3").children().hide();
     $("#sec_box_2").show();
   });
 
   $("#sec_sub3").click(function(){
+    $("#second_3").children().hide();
     $("#sec_box_3").show();
   });
 
   $("#sec_sub4").click(function(){
+    $("#second_3").children().hide();
     $("#sec_box_4").show();
   });
 
